8001967bfed9bc56f3f11f972d6632d63120e91e
jamesndukwe <james.ag2001@gmail.com>
2016-08-22 13:33:36 +0100

    b

--- js/fapiScript.js
+++ js/fapiScript.js
@@ -133,7 +133,7 @@ var APP_ID;
                 //some code
             } else if (response.status === 'unknown') {
                 FB.login(function (response) {
-                    statusChangeCallback(response);
+                    statusChangeCallback(response);alert(response.status)
                 });
             }
 	}  
